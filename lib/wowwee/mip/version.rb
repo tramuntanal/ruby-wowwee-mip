972c00705ebebf1afd1e75c8383bc27c2c2f2075
oliver <oliver.hv@coditramuntana.com>
2016-11-01 23:35:23 +0100

    Changelog mispelling.

--- lib/wowwee/mip/version.rb
+++ lib/wowwee/mip/version.rb
@@ -4,7 +4,7 @@ module Wowwee
     # Major version zero (0.y.z) is for initial development. Anything may change at any time. The public API should not be considered stable.
 
     # MINOR 0.2.0
-    # - Refactorized protocol implementation. Moved from a class
+    # - Refactor protocol implementation. Moved from a class
     # per command idea to a configuralbe unique Cmd class.
     VERSION = '0.2.0'.freeze
   end
