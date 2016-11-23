84a89a45f8623cc0967418ecfb504a7c230abbb2
oliver <oliver.hv@coditramuntana.com>
2016-11-23 23:34:57 +0100

    Update changelog.

--- lib/wowwee/mip/version.rb
+++ lib/wowwee/mip/version.rb
@@ -2,10 +2,15 @@ module Wowwee
   module Mip
     # Semantic Versioning: http://semver.org/
     # Major version zero (0.y.z) is for initial development. Anything may change at any time. The public API should not be considered stable.
+    VERSION = '0.3.0'.freeze
+
+    # MINOR 0.3.0
+    # [REFACTOR] Itroduce NotifQueue to replace former callback for receiveing BLE notifications.
+    # [FEATURE] Introduce the robot controller to manage some robot functionality.
+
 
     # MINOR 0.2.0
     # - Refactor protocol implementation. Moved from a class
     # per command idea to a configuralbe unique Cmd class.
-    VERSION = '0.2.0'.freeze
   end
 end
